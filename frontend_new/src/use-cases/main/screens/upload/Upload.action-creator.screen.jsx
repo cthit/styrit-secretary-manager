1fb5b77c73124f7c376e0ee9c5e7c7e222315810
Vidar Magnusson <vidar.halmstad@hotmail.com>
2020-02-04 17:21:53 +0100

    Now able to leave file upload screen.

--- frontend_new/src/use-cases/main/screens/upload/Upload.action-creator.screen.jsx
+++ frontend_new/src/use-cases/main/screens/upload/Upload.action-creator.screen.jsx
@@ -1,70 +1,74 @@
 import {
-    ON_UPLOAD,
-    ON_SUBMIT_FILES_FAILED,
-    ON_FILEUPLOAD_FINISHED
+  ON_UPLOAD,
+  ON_SUBMIT_FILES_FAILED,
+  ON_FILEUPLOAD_FINISHED
 } from "./Upload.actions.screen";
 import { putFiles } from "../../../../api/put.Files.api";
 import { handleError } from "../../../../common/functions/handleError";
 import { DigitDialogActions } from "@cthit/react-digit-components";
 
 export function onUpload(file, task) {
-    return {
-        type: ON_UPLOAD,
-        payload: {
-            file: file,
-            task: task
-        },
-        error: false
-    };
+  return {
+    type: ON_UPLOAD,
+    payload: {
+      file: file,
+      task: task
+    },
+    error: false
+  };
 }
 
 export function onSubmitFiles(reports, code, group) {
-    return dispatch => {
-        putFiles(reports, code, group)
-            .then(response => {
-                return dispatch(onAccept(response));
-            })
-            .catch(error => {
-                return dispatch(onError(error));
-            });
-    };
+  return dispatch => {
+    putFiles(reports, code, group)
+      .then(response => {
+        return dispatch(onAccept(response));
+      })
+      .catch(error => {
+        return dispatch(onError(error));
+      });
+  };
 }
 
 function onAccept(response) {
-    console.log("RESPONSE::", response);
+  console.log("RESPONSE::", response);
 
-    let overwrite = false;
-    if (response.data) {
-        overwrite = response.data.overwrite;
-    }
+  let overwrite = false;
+  if (response.data) {
+    overwrite = response.data.overwrite;
+  }
 
-    let msg = "";
-    if (overwrite) {
-        msg = "\nSkrev över tidigare uppladdad fil.";
-    }
+  let msg =
+    "Om du vill byta ut en fil är det bara att skriva in koden igen och ladda upp en ny fil.";
+  if (overwrite) {
+    msg = msg + "\nSkrev över tidigare uppladdad fil.";
+  }
 
-    let dialogData = {
-        title: "Fil(er) upladdade!",
-        description:
-            "Om du vill byta ut en fil är det bara att skriva in koden igen och ladda upp en ny fil." +
-            msg,
-        confirmButtonText: "Ok",
-        cancelButtonText: "Fortsätt ladda upp filer med samma kod ",
-        onConfirm: () => onConfirmFilesUploaded
-    };
+  alert(msg);
 
-    return DigitDialogActions.digitDialogOpen(dialogData);
+  return onConfirmFilesUploaded();
+
+  // Add this later if we can get it to work.
+  let dialogData = {
+    title: "Fil(er) upladdade!",
+    description: msg,
+    confirmButtonText: "Ok",
+    cancelButtonText: "Fortsätt ladda upp filer med samma kod ",
+    onConfirm: () => onConfirmFilesUploaded
+  };
+
+  return DigitDialogActions.digitDialogOpen(dialogData);
 }
 
 function onError(error) {
-    return handleError(error, ON_SUBMIT_FILES_FAILED);
+  return handleError(error, ON_SUBMIT_FILES_FAILED);
 }
 
 function onConfirmFilesUploaded() {
-    // Reset the state to code input.
-    return {
-        type: ON_FILEUPLOAD_FINISHED,
-        payload: {},
-        error: false
-    };
+  // Reset the state to code input.
+  return {
+    type: ON_FILEUPLOAD_FINISHED,
+    payload: {},
+    error: false
+  };
 }
