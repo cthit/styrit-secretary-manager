95023c1ec33ec5e1d809f72bb1aca18c13006999
Vidar Magnusson <git@vidarmagnusson.com>
2020-05-30 18:52:08 +0200

    Prepared for being restored from backup

--- backend/src/app.py
+++ backend/src/app.py
@@ -9,10 +9,9 @@ from src.db import drop_db
 from src.db_migration_tool import backup_db, restore_to_new_db
 
 if __name__ == '__main__':
+    drop_db()
+    restore_to_new_db()
     setup.setup_db()
-#    drop_db()
-#    backup_db()
 
-#    restore_to_new_db()
     threading.Thread(target=end_date_handler.check_for_enddate).start()
     threading.Thread(target=web_handler.host).start()
