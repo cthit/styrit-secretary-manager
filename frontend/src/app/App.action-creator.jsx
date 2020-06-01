6ffd00ba57d175e25541002aa670b31bcceda8a7
Vidar Magnusson <git@vidarmagnusson.com>
2020-06-01 18:20:11 +0200

    Fixed debug mode env variable in frontend

--- frontend/src/app/App.action-creator.jsx
+++ frontend/src/app/App.action-creator.jsx
@@ -1,8 +1,14 @@
-import { INIT } from "./App.actions";
-import { initApi } from "../api/RequestUtilities";
+import {INIT} from "./App.actions";
+import {initApi} from "../api/RequestUtilities";
 
 export function init() {
-    let debug = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+//    let debug = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+    let debug = false
+
+    if (process.env.REACT_APP_DEBUG_MODE) {
+        debug = process.env.REACT_APP_DEBUG_MODE;
+    }
+
     initApi(debug);
     return {
         type: INIT,
