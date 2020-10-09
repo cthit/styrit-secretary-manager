55346b2ece0581065d0519e1bb66806221c188a9
Vidar Magnusson <git@vidarmagnusson.com>
2020-10-09 18:00:09 +0200

    Fix typing bug

--- backend/src/command/MeetingCommands.py
+++ backend/src/command/MeetingCommands.py
@@ -1,12 +1,13 @@
-import uuid
+
 from datetime import datetime
+from uuid import UUID
 
 from ResultWithData import ResultWithData, get_result_with_error, get_result_with_data
 from data_objects.MeetingJsonData import MeetingJsonData
 from db import Meeting
 
 
-def create_new_meeting(date: datetime, last_upload: datetime, lp: int, meeting_no: int) -> ResultWithData[uuid]:
+def create_new_meeting(date: datetime, last_upload: datetime, lp: int, meeting_no: int) -> ResultWithData[UUID]:
     try:
         meeting = Meeting(year=datetime.year, date=date, last_upload=last_upload, lp=lp, meeting_no=meeting_no, check_for_deadline=False)
     except Exception as e:
