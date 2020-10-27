888f3834c9ad530614a1de45d0b7a77a747cbc19
Vidar Magnusson <git@vidarmagnusson.com>
2020-10-27 17:08:37 +0100

    Add 'unsaved changes' warning to general config page

--- frontend/src/use-cases/admin/screens/config/views/general/General.container.view.jsx
+++ frontend/src/use-cases/admin/screens/config/views/general/General.container.view.jsx
@@ -10,13 +10,15 @@ const mapStateToProps = state => ({
     configs: state.root.GeneralReducer.configs,
     password: state.root.PasswordReducer.password,
     selectedConfigIndex: state.root.GeneralReducer.selectedHelpIndex,
-    selectedHelp: state.root.GeneralReducer.selectedHelp
+    selectedHelp: state.root.GeneralReducer.selectedHelp,
+    unsavedChanges: state.root.GeneralReducer.unsavedChanges
 });
 
 const mapDispatchToProps = dispatch => ({
-    onConfigChange: (key, newVal) => dispatch(onConfigChange(key, newVal)),
-    onConfigSave: (password, configs) => saveConfig(password, configs),
-    configHelpButtonPressed: index => dispatch(configHelpButtonPressed(index))
-});
+        onConfigChange: (key, newVal) => dispatch(onConfigChange(key, newVal)),
+        onConfigSave: (password, configs) => dispatch(saveConfig(password, configs)),
+        configHelpButtonPressed: index => dispatch(configHelpButtonPressed(index))
+    })
+;
 
 export default connect(mapStateToProps, mapDispatchToProps)(General);
